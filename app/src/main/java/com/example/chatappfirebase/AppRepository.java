b83b3ee53f4f966a3391dd02b7d4f1b982c59c05
Chirag patel <chirag.patel@mobiuso.com>
2022-04-01 16:22:11 +0530

    Login screen fragment added

---
+++ app/src/main/java/com/example/chatappfirebase/AppRepository.java
@@ -0,0 +1,4 @@
+package com.example.chatappfirebase;
+
+public class AppRepository {
+}
---
+++ app/src/main/java/com/example/chatappfirebase/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.chatappfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+/*
+Bundle[
+        {InAppSecondaryButtonText=Close,
+        InAppImagePortraitUrl=https://s3.amazonaws.com/cdn.skyscape.com/images/Emails/Springy2022_1.png,
+        InAppTitle=20220316_Test01,
+        InAppExpirationDateStamp=20220307,
+        InAppPrimaryButtonText=SHOP NOW,
+        InAppDescription=Use code: SPRINGY2022. Save now through 4/5/22,
+        InAppImageLandscapeUrl=https://s3.amazonaws.com/cdn.skyscape.com/images/Emails/Springy2022_2.png,
+        InAppPrimaryButtonAction=medpresso://skyscape?login#url=https://www.skyscape.com/estore/fullproductlisting.aspx?utm_source=FRB&utm_medium=PINAPP&utm_content=PROMO&utm_campaign=SPRINGY20220316}]
+
+*/
